055b5b025b686329a47a2de16301f4b9828b4199
Alyssa Anastasi <alyssaa5@illinois.edu>
2024-09-19 02:30:44 -0500

    lowkey final website

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
 
 function App() {
   return (
@@ -12,9 +9,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
