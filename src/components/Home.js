62e9cdda07271aca4dd6be398c56de1b3d3372a5
Alyssa Anastasi <alyssaa5@illinois.edu>
2025-08-11 14:54:43 -0500

    small stylings 2

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,11 +13,11 @@ function Home() {
       <h2>BS Statistics & Computer Science from UIUC</h2>
       <h3><b>Currently:</b></h3>
       <ul>
-        <li>Located in: <span className="bullet-text">Madison, WI</span></li>
-        <li>Working on: <span className="bullet-text">optimizing vaccination rates in Illinois in a "tripledemic" season</span></li>
-        <li>Reading: <a href="https://www.goodreads.com/book/show/218559595-great-big-beautiful-life">Great Big Beautiful Life by Emily Henry</a> </li>
-        <li>Listening to: <a href="https://open.spotify.com/track/4NczzeHBQPPDO0B9AAmB8d?si=99bfb5da6e1b4e2f">Assumptions by Sam Gellaitry</a> </li>
-        <li>Obsessed with: <a href="https://strava.app.link/cR1DpRqHQTb">running & biking</a> </li>
+        <li>Located in <span className="bullet-text">Madison, WI</span></li>
+        <li>Working on <span className="bullet-text">optimizing vaccination rates in Illinois in a "tripledemic" season</span></li>
+        <li>Reading <a href="https://www.goodreads.com/book/show/218559595-great-big-beautiful-life">Great Big Beautiful Life by Emily Henry</a> </li>
+        <li>Listening to <a href="https://open.spotify.com/track/4NczzeHBQPPDO0B9AAmB8d?si=99bfb5da6e1b4e2f">Assumptions by Sam Gellaitry</a> </li>
+        <li>Obsessed with <a href="https://strava.app.link/cR1DpRqHQTb">running & biking</a> </li>
       </ul>
       </div>
     </div>
