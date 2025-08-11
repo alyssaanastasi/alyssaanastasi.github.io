c108e97a31ee06a5164363bf796912145a8b56b7
Alyssa Anastasi <alyssaa5@illinois.edu>
2025-08-11 14:09:36 -0500

    final elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ function Home() {
       <h3><b>Currently:</b></h3>
       <ul>
         <li>Located in: <span className="bullet-text">Madison, WI</span></li>
-        <li>Working on: <span className="bullet-text">predicting which animal viruses are at high risk of spilling over into human populations in mark craven's lab</span></li>
+        <li>Working on: <span className="bullet-text">optimizing vaccination rates in Illinois in a "tripledemic" season</span></li>
         <li>Reading: <a href="https://www.goodreads.com/book/show/218559595-great-big-beautiful-life">Great Big Beautiful Life by Emily Henry</a> </li>
         <li>Listening to: <a href="https://open.spotify.com/track/4NczzeHBQPPDO0B9AAmB8d?si=99bfb5da6e1b4e2f">Assumptions by Sam Gellaitry</a> </li>
         <li>Obsessed with: <a href="https://strava.app.link/cR1DpRqHQTb">running & biking</a> </li>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,13 +5,28 @@ function Projects() {
   return (
     <div className="projects-text">
       <h1>Projects</h1>
-      <h2>Optimizing vaccination rates in a RSV, Influenza, and COVID “tripledemic”</h2>
-      <h3>UIUC &#x2022; PI: Pamela Martinez &#x2022; Jan. 2025 - Present</h3>
+      {/* <h2>Analyzing the Risk of Zoonotic Virus Spill Over into Human Populations</h2>
+      <h3>UW-Madison &#x2022; PI: Mark Craven &#x2022; Sep. 2025 - Present </h3>
+      <p>In this project, ...</p> */}
+      <h2>Optimizing Vaccination Rates in a RSV, Influenza, and COVID “Tripledemic”</h2>
+      <h3>UIUC &#x2022; PIs: Soren Larsen & Pamela Martinez &#x2022; Jan. 2025 - Present</h3>
+      <p>The aim of this project is to characterize the optimal age-based distribution of vaccines for RSV, influenza, and
+         COVID-19 in a "tripledemic" season with finite healthcare resources. I developed an SIR (Susceptible-Infected-Recovered) 
+         model in R to simulate disease transmission dynamics across these three viruses simultaneously. By analyzing how different 
+         vaccination coverages across age groups influence infection peaks and overall disease burden, this work provides insights 
+         into targeted vaccine allocation in Illinois.</p>
       <h2>Assessing explanations of graph neural networks for predicting effects of gene perturbations</h2>
       <h3>UW-Madison &#x2022; PI: Mark Craven &#x2022; Summer 2024 &#x2022; <a href={gnnPoster} target="_blank">Poster</a></h3>
-      <p></p>
+      <p>In this project, I worked with a graph neural network (GNN) that integrated diverse biological 
+        to predict how gene perturbations affect gene expression, specifically in looking at the phenotype of cholesterol homeostasis. 
+        To better understand the model’s decisions, I applied GNN explanation methods including GNNExplainer, PGExplainer, & Graph Mask
+        using PyTorch Geometric to evaluate the model's reliability. </p>
       <h2>Disentangling the impact of environmental drivers on Rotavirus transmission in Bangladesh</h2>
       <h3>UIUC &#x2022; PI: Pamela Martinez &#x2022; Aug. 2023 - May 2024</h3>
+      <p> The goal of this project is to understand how climate factors including flooding, temperature, & humidity can predict rotavirus
+        incidence across the different districts of Bangladesh. I conducted extensive data cleaning and preprocessing of multiple data 
+        sources including rotavirus case data, climate data, and flooding data in R.
+        I then applied generalized linear models to identify key climate factors influencing rotavirus incidence in Bangladesh.</p>
     </div>
     );
 }
