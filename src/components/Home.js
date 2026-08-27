e14f3fb565cadb12089219dd6e826b8f4e00bf21
Alyssa Anastasi <alyssarose91402@gmail.com>
2026-08-27 14:21:00 -0500

    Change current & previous

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,15 +13,15 @@ function Home() {
       <h2>BS Statistics & Computer Science from UIUC</h2>
       <h3><b>Currently:</b></h3>
       <ul>
-        <li><span className="bullet-text">Data Science Intern @ Gilead Sciences</span></li>
         <li><span className="bullet-text">Graduate Research Assistant in Mark Craven's Lab</span></li>
         <li>Reading <a href="https://www.goodreads.com/en/book/show/208931300-intermezzo">Intermezzo by Sally Rooney</a> </li>
         <li>Listening to <a href="https://open.spotify.com/album/2PfGKHtqEX58bHtkQxJnWG?si=aMAs59PeTe-xBcKZjRe1Qw">Love Deluxe by Sade</a> </li>
-        <li>Obsessed with <a href="https://ebird.org/profile/ODk5NjAxNw">finding cool birds</a></li>
-        <li>Located in <span className="bullet-text">San Mateo, CA</span></li>
+        <li>Obsessed with <a href="https://www.learnedleague.com/">Learned League trivia</a></li>
+        <li>Located in <span className="bullet-text">Madison, WI</span></li>
       </ul>
       <h3><b>Previously:</b></h3>
       <ul>
+        <li>Data Science Intern @ Gilead Sciences</li>
         <li>Undergraduate Research Assistant in Pamela Martinez's Lab @ UIUC</li>
         <li>Data Science Instructor @ Discovery Partners Institute</li>
         <li>Quality Analyst Intern @ AbbVie</li>
